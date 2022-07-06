927e4f42bbe83f430b03b687fa2db4e52b72976b
tjs6595 <t.schultejans@schenckprocess.com>
2022-07-05 22:28:48 -0500

    React Rainbow Submission - without Bonus.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { useState } from 'react'
+
+// Make sure to import the component we just built:
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+function App(){
+    let [colors, setColors] = useState(['violet','blue','lightblue','green','greenyellow','yellow','orange','red']);
+    let colorMap = colors.map((color, i) => {
+        return (
+          <div className="App">
+            <ColorBlock key={i} color={color}/>
+          </div>
+        )
+    })
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor}/>
+        </div>
+    )
 }
 
 export default App;
